38b078402dcc8a08fd3f72f06be5e5857d4c7672
liach <liangchenblue@gmail.com>
2015-02-03 20:12:19 -0800

    mod init

---
+++ src/main/java/goodtime/industrial/gti.java
@@ -0,0 +1,10 @@
+package goodtime.industrial;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid=gti.modinfo,name=gti.modinfo,version=gti.version,dependencies="IC2",acceptedMinecraftVersions=gti.mcversion)
+public final class gti{
+    public static final String modinfo="GoodTime-Industrial";
+    public static final String version="0.0.1";
+    public static final String mcversion="1.7.10";
+}
