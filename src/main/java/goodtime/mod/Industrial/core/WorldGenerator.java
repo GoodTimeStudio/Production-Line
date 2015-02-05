dd74e084effb8cdde7b2f1cf4b2327cceed5ebd3
Su Hao <1156407698@qq.com>
2015-02-05 10:40:43 +0800

    WorldGen

---
+++ src/main/java/goodtime/mod/Industrial/core/WorldGenerator.java
@@ -0,0 +1,31 @@
+package goodtime.mod.Industrial.core;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import cpw.mods.fml.common.IWorldGenerator;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class WorldGenerator  implements IWorldGenerator{
+
+	private WorldGenMinable IRGenerator;
+	
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world,
+			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+		
+	}
+	
+	public void generateSurface(World world, java.util.Random rand, int chunkX, int chunkZ){
+		for(int i = 0; i < 2; i++){
+			int randPosX = chunkX + rand.nextInt(16);
+			int randPosY = rand.nextInt(40)+7;
+			int randPosZ = chunkZ + rand.nextInt(16);
+			(new WorldGenMinable(Ore.IR, 32)).generate(world, rand, randPosX, randPosY, randPosZ);
+		}
+	}
+}
