3f2db8d84c0a574bc0044180a2780ec35a675afb
Su Hao <1156407698@qq.com>
2015-03-31 13:52:48 +0800

    Rename container to inventory

--- src/main/java/com/mcgoodtime/gti/common/inventory/ContainerGenGasKU.java
+++ src/main/java/com/mcgoodtime/gti/common/inventory/ContainerGenGasKU.java
@@ -1,15 +1,10 @@
-package com.mcgoodtime.gti.common.container;
+package com.mcgoodtime.gti.common.inventory;
 
 import com.mcgoodtime.gti.common.tiles.TileGenGasKu;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
-import net.minecraft.inventory.ICrafting;
 import net.minecraft.inventory.Slot;
-import net.minecraft.inventory.SlotFurnace;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.FurnaceRecipes;
-import net.minecraft.tileentity.TileEntityFurnace;
 
 public class ContainerGenGasKU extends Container {
 
