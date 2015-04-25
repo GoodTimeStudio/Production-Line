7e9d74bab2705e273f8beef2ceb99c7398b24fbc
liach <liangchenblue@gmail.com>
2015-04-25 14:09:04 -0700

    recode IridiumGen.java

--- src/main/java/com/mcgoodtime/gti/common/worldgen/IridiumGen.java
+++ src/main/java/com/mcgoodtime/gti/common/worldgen/IridiumGen.java
@@ -22,40 +22,38 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package com.mcgoodtime.gti.common.worldgen;
 
-import com.mcgoodtime.gti.common.blocks.OreIridium;
-
 import com.mcgoodtime.gti.common.init.GtiBlocks;
+
 import cpw.mods.fml.common.IWorldGenerator;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 
 import java.util.Random;
 
-public class IridiumGen implements IWorldGenerator{
+/**
+ * The class for Iridium WorldGen.
+ *
+ * @author Su Hao
+ */
+public class IridiumGen implements IWorldGenerator {
 
-	@Override
-	public void generate(Random random, int chunkX, int chunkZ, World world,
-			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
-		generateSurface(world, random, chunkX, chunkZ);
-	}
-	
-	public static void generateSurface(World world, Random random, int chunkX, int chunkZ)
-	{
-		for (int i = 0; i < 5; i++) {
-			int randPosX = chunkX * 16 + random.nextInt(16);
-			int randPosY = random.nextInt(16);
-			int randPosZ = chunkZ * 16 + random.nextInt(16);
-			new WorldGenMinable(GtiBlocks.oreIridium, 50).generate(world, random, randPosX, randPosY, randPosZ);
-		}
+    private static final int TICKET = 5;
+    private static final int MAX_HEIGHT = 16;
 
-	}
-/*
-	public static void preInit(FMLPreInitializationEvent event) {
-		MinecraftForge.ORE_GEN_BUS.register(this);
-	}*/
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world,
+                         IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+        generateIridium(random, world, chunkX, chunkZ);
+    }
+    public static void generateIridium(Random random, World world, int chunkX, int chunkZ) {
+        for (int i = 0; i < TICKET; i++) {
+            int x = chunkX * 16 + random.nextInt(16);
+            int z = chunkZ * 16 + random.nextInt(16);
+            int y = random.nextInt(MAX_HEIGHT);
+            new WorldGenMinable(GtiBlocks.oreIridium, 50).generate(world, random, x, y, z);
+        }
+    }
 }
