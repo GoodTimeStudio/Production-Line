1c2c46a3b2faedb4e08e91f307e6a9518574cc26
liach <liangchenblue@gmail.com>
2015-04-29 17:40:21 -0700

    Removed Su Hao's code, @1156407698 should do it like the one on https://github.com/CovertJaguar/Railcraft/blob/master/src/main/java/mods/rai

--- src/main/java/com/mcgoodtime/gti/common/core/config/GtiConfig.java
+++ src/main/java/com/mcgoodtime/gti/common/core/config/GtiConfig.java
@@ -25,13 +25,14 @@
 package com.mcgoodtime.gti.common.core.config;
 
 import com.mcgoodtime.gti.common.core.Gti;
+
+import cpw.mods.fml.common.FMLCommonHandler;
 import net.minecraftforge.common.config.Configuration;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.*;
-import java.util.Properties;
+import java.io.File;
 
 /**
  * Config loader in GoodTime-Industrial.
@@ -43,8 +44,6 @@ public class GtiConfig {
     public static Configuration gtiConfig;
     public static File configFile;
 
-    private static Properties cfgProp = new Properties();
-
     public static void init() {
         configFile = new File("config", Gti.MOD_NAME + ".cfg");
 
@@ -54,67 +53,12 @@ public class GtiConfig {
         }
 
         gtiConfig = new Configuration(configFile);
-        gtiConfig.load();
-        gtiLogger.log(Level.INFO, "Gti logger loaded");
-    }
-
-    // load the config file
-    public static String getConfig(String configKey) {
-        String key = null;
-        try {
-            FileInputStream in = new FileInputStream(configFile);
-            InputStreamReader isr = new InputStreamReader(in, "UTF-8");
-            cfgProp.load(isr);
-            key = cfgProp.getProperty(configKey);
-            isr.close();
-            in.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (!configFile.exists()) {
+            gtiLogger.log(Level.ERROR, "Cannot create Gti config file");
+            gtiLogger.log(Level.INFO, "Skipping config load");
+        } else {
+            gtiConfig.load();
         }
-        return key;
-    }
-
-    // load the config file
-    public static Boolean getConfigAsBoolean(String configKey) {
-        Boolean key = null;
-        try {
-            FileInputStream in = new FileInputStream(configFile);
-            InputStreamReader isr = new InputStreamReader(in, "UTF-8");
-            cfgProp.load(isr);
-            String stringKey = cfgProp.getProperty(configKey);
-
-            if (stringKey.equals("true")) key = true;
-            else key = false;
-
-            isr.close();
-            in.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return key;
-    }
-
-    // add to the config file
-    public static void setConfig(String configKey, String value) {
-        cfgProp.setProperty(configKey, value);
-    }
-
-    public static void saveConfig() {
-        try {
-            OutputStream out = new FileOutputStream(configFile);
-            cfgProp.store(out, "# Configuration file\n# GoodTime-Industrial");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        gtiLogger.log(Level.INFO, "Gti config loaded");
     }
 }
