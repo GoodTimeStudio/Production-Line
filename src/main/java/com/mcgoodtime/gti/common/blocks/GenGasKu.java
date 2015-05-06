669123670250a28c365e1e4898b531d60f0972c5
liach <liangchenblue@gmail.com>
2015-05-05 17:28:03 -0700

    Fixed that weird issue on rename

---
+++ src/main/java/com/mcgoodtime/gti/common/blocks/GenGasKu.java
@@ -0,0 +1,67 @@
+/*
+ * This file is part of GoodTime-Industrial, licensed under MIT License (MIT).
+ *
+ * Copyright (c) 2015 Minecraft-GoodTime <http://github.com/Minecraft-GoodTime>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.mcgoodtime.gti.common.blocks;
+
+import static com.mcgoodtime.gti.common.core.CreativeTabGti.creativeTabGti;
+
+import com.mcgoodtime.gti.common.core.Gti;
+import com.mcgoodtime.gti.common.core.GuiHandler;
+import com.mcgoodtime.gti.common.tiles.TileGenGasKu;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class GenGasKu extends BlockContainer {
+    public GenGasKu() {
+        super(Material.rock);
+        this.setCreativeTab(creativeTabGti);
+        this.setBlockName("gti.GenGasKu");
+        this.setBlockTextureName("gti:GenGasKu");
+        GameRegistry.registerBlock(this, "GenGasKu");
+    }
+
+    /**
+     * Called upon blocks activation (right click on the blocks.)
+     */
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer entityPlayer, int p1, float p2, float p3, float p4) {
+        if (world.isRemote) {
+            entityPlayer.openGui(Gti.instance, GuiHandler.Guis.GenGasKu.ordinal(), world, x, y, z);
+        } else {
+            entityPlayer.isInvisibleToPlayer(entityPlayer);
+        }
+        return true;
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World world, int var2) {
+        return new TileGenGasKu();
+    }
+
+}
