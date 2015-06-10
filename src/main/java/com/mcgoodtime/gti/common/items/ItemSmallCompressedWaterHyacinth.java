2a66fbfdde22efcd811c0fb48f7df66e292565b8
Su Hao <1156407698@qq.com>
2015-06-10 19:59:20 +0800

    some change

--- src/main/java/com/mcgoodtime/gti/common/items/ItemSmallCompressedWaterHyacinth.java
+++ src/main/java/com/mcgoodtime/gti/common/items/ItemSmallCompressedWaterHyacinth.java
@@ -6,7 +6,7 @@ import static com.mcgoodtime.gti.common.core.Gti.creativeTabGti;
 
 public class ItemSmallCompressedWaterHyacinth extends Item{
 	public ItemSmallCompressedWaterHyacinth(){
-		this.setUnlocalizedName("gti.item.SmallCompressedWaterHyacinth");
+		this.setUnlocalizedName("gti.item.smallCompressedWaterHyacinth");
 		this.setTextureName("gti:itemSmallCompressedWaterHyacinth");
 		this.setMaxStackSize(64);
 		this.setCreativeTab(creativeTabGti);
