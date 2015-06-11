3951f38582743711d3bc598ea8a6564d507d4a46
liach <liach@users.noreply.github.com>
2015-06-11 10:13:10 -0700

    Haha soooo buggy
    I did it w/o the compiler, fix it plz!

--- src/main/java/com/mcgoodtime/gti/common/worldgen/BsaltGen.java
+++ src/main/java/com/mcgoodtime/gti/common/worldgen/BsaltGen.java
@@ -12,31 +12,30 @@ import ic2.api.item.IC2Items;
 import java.util.Random;
 
 /**
- * 
+ * Another world generator?
  *
  * @author JAVA10
  */
 
-public class BsaltGen implements IWorldGenerator{
-	private static final int TICKET=50;
-	private static final int MAX_HEIGHT=27;
-	private static final int GEN_SIZE=10;
+public class BsaltGen implements IWorldGenerator {
+    private static final int TICKET = 5;
+    private static final int MAX_HEIGHT = 27;
+    private static final int GEN_SIZE = 10;
 	
-	@Override
-	public void generate(Random random, int chunkX, int chunkZ, World world,
-			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
-	if((!world.provider.isHellWorld) && (!world.provider.hasNoSky)){
-		generateOre(world, random, chunkX, chunkZ);
-	} 	
-}
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world,
+            IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+        if (!world.provider.isHellWorld && !world.provider.hasNoSky)
+           generateOre(world, random, chunkX, chunkZ);
+    }
 
-	private void generateOre(World world, Random rand, int chunkX, int chunkZ) {
-		for (int k = 0; k < this.TICKET; k++) {
-            int x = chunkX * 27 + rand.nextInt(27);
+    private void generateOre(World world, Random rand, int chunkX, int chunkZ) {
+        for (int k = 0; k < this.TICKET; k++) {
+            int x = chunkX * 16 + rand.nextInt(16);
             int y = rand.nextInt(this.MAX_HEIGHT);
-            int z = chunkZ * 27+ rand.nextInt(27);
+            int z = chunkZ * 16 + rand.nextInt(16);
 
-            new WorldGenMinable(Block.getBlockFromItem(IC2Items.getItem("Basalt").getItem()), this.GEN_SIZE).generate(world, rand, x, y, z);
+            new WorldGenMinable(IC2Items.getItem("Basalt")), this.GEN_SIZE).generate(world, rand, x, y, z);
         }
-	}
+    }
 }
