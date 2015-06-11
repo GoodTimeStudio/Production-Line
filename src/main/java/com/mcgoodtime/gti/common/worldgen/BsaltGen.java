558cbe08de3444a22c2a8d72b649710bcbb1958b
Java0 <563622979@qq.com>
2015-06-11 20:18:58 +0800

    BasaltGen

---
+++ src/main/java/com/mcgoodtime/gti/common/worldgen/BsaltGen.java
@@ -0,0 +1,42 @@
+package com.mcgoodtime.gti.common.worldgen;
+
+
+import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import ic2.api.item.IC2Items;
+
+import java.util.Random;
+
+/**
+ * 
+ *
+ * @author JAVA10
+ */
+
+public class BsaltGen implements IWorldGenerator{
+	private static final int TICKET=50;
+	private static final int MAX_HEIGHT=27;
+	private static final int GEN_SIZE=10;
+	
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world,
+			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+	if((!world.provider.isHellWorld) && (!world.provider.hasNoSky)){
+		generateOre(world, random, chunkX, chunkZ);
+	} 	
+}
+
+	private void generateOre(World world, Random rand, int chunkX, int chunkZ) {
+		for (int k = 0; k < this.TICKET; k++) {
+            int x = chunkX * 27 + rand.nextInt(27);
+            int y = rand.nextInt(this.MAX_HEIGHT);
+            int z = chunkZ * 27+ rand.nextInt(27);
+
+            new WorldGenMinable(Block.getBlockFromItem(IC2Items.getItem("Basalt").getItem()), this.GEN_SIZE).generate(world, rand, x, y, z);
+        }
+	}
+}
