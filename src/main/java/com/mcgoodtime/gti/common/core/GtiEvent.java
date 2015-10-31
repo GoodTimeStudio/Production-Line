7a479976582533d0ed10a8534d6e671b2c80522a
Su Hao <1156407698@qq.com>
2015-10-31 21:14:30 +0800

    cleanup code

--- src/main/java/com/mcgoodtime/gti/common/core/GtiEvent.java
+++ src/main/java/com/mcgoodtime/gti/common/core/GtiEvent.java
@@ -30,7 +30,6 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
 
 /*
  * Created by suhao on 2015/5/17.
