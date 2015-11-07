78577543a1189b94aba89db3e4ff3461ab4b99c3
Su Hao <1156407698@qq.com>
2015-11-07 19:58:52 +0800

    Spawn salt when throw packagedSalt

--- src/main/java/com/mcgoodtime/gti/common/entity/EntityPackagedSalt.java
+++ src/main/java/com/mcgoodtime/gti/common/entity/EntityPackagedSalt.java
@@ -2,18 +2,15 @@ package com.mcgoodtime.gti.common.entity;
 
 import com.mcgoodtime.gti.common.GtiPotion;
 import com.mcgoodtime.gti.common.init.GtiItems;
-import ic2.core.IC2Potion;
-import ic2.core.Ic2Items;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
-
-import java.util.Random;
 
 /**
  * Created by BestOwl on 2015.11.4.0004.
@@ -40,25 +37,24 @@ public class EntityPackagedSalt extends EntityThrowable {
     @Override
     protected void onImpact(MovingObjectPosition objectPosition) {
         if (objectPosition.entityHit != null) {
-            byte b0 = 0;
-
-            if (objectPosition.entityHit instanceof EntityBlaze) {
-                b0 = 3;
-            }
-
-            objectPosition.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), (float)b0);
+            objectPosition.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), 3F);
             if (objectPosition.entityHit instanceof EntityLivingBase) {
                 ((EntityLivingBase) objectPosition.entityHit).addPotionEffect(new PotionEffect(GtiPotion.salty.id, 0, 3));
             }
         }
 
-        Random random = new Random();
         for (int i = 0; i < 8; ++i) {
-            float fmod = -0.1F - random.nextFloat() * 1.2F;
-            float f1mod = -0.05F + random.nextFloat() * 0.4F;
-            float f2mod = -0.1F - random.nextFloat() * 1.2F;
-            this.worldObj.spawnParticle("iconcrack_" + Item.getIdFromItem(GtiItems.salt), this.posX + 1.0F + fmod,
-                    this.posY + 1.0F + f1mod, this.posZ + 1.0F + f2mod, 0.1D, 0.1D, 0.1D);
+            float fmod = (float) (1.2 - (Math.random() * 2.4));
+            float f1mod = (float) (0.5 - (Math.random() * 1.0));
+            float f2mod = (float) (1.2 - (Math.random() * 2.4));
+
+            this.worldObj.spawnParticle("iconcrack_" + Item.getIdFromItem(GtiItems.salt), this.posX + fmod,
+                    this.posY + f1mod, this.posZ + f2mod, 0.1D, 0.1D, 0.1D);
+
+            if (!this.worldObj.isRemote) {
+                EntityItem entityItem = new EntityItem(this.worldObj, this.posX + fmod, this.posY + f1mod, this.posZ + f2mod, new ItemStack(GtiItems.salt));
+                this.worldObj.spawnEntityInWorld(entityItem);
+            }
         }
 
         if (!this.worldObj.isRemote) {
