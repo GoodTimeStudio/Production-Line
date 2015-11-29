9855776678533f1ab9ad37945241ace9c935460d
BestOwl <1156407698@qq.com>
2015-11-29 14:21:08 +0800

    Add useful method

--- src/main/java/com/mcgoodtime/gti/GtiUtil.java
+++ src/main/java/com/mcgoodtime/gti/GtiUtil.java
@@ -2,6 +2,8 @@ package com.mcgoodtime.gti;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatComponentTranslation;
 
 /**
  * Created by BestOwl on 2015.11.14.0014.
@@ -13,5 +15,10 @@ public class GtiUtil {
     @SideOnly(Side.CLIENT)
     public static int getGuiScaled(int scale, float min, float max) {
         return (int) (scale * Math.min(1.0F, min / max));
+    }
+
+    public static void messageToPlayer(EntityPlayer player, String message, Object... args) {
+        ChatComponentTranslation msg = new ChatComponentTranslation(message, args);
+        player.addChatComponentMessage(msg);
     }
 }
