cce3e78c78cec66f59530e916bbf9a3ed857a1e4
liach <liach@users.noreply.github.com>
2016-10-13 20:24:30 -0700

    2 items

--- src/main/java/com/mcgoodtime/productionline/common/items/ItemElectricPL.java
+++ src/main/java/com/mcgoodtime/productionline/common/items/ItemElectricPL.java
@@ -1,14 +1,13 @@
-/*
 package com.mcgoodtime.productionline.common.items;
 
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import ic2.api.energy.EnergyNet;
 import ic2.api.item.ElectricItem;
 import ic2.api.item.IElectricItem;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.List;
 
@@ -16,7 +15,7 @@ import java.util.List;
  * Created by BestOwl on 2015.12.8.0008.
  *
  * @author BestOwl
- *//*
+ */
 public class ItemElectricPL extends ItemPL implements IElectricItem {
 
     protected int maxPowerTick;
@@ -37,52 +36,30 @@ public class ItemElectricPL extends ItemPL implements IElectricItem {
     @Override
     @SideOnly(Side.CLIENT)
     public void getSubItems(Item item, CreativeTabs par2CreativeTabs, List itemList) {
-        ItemStack itemStack = new ItemStack(this, 1);
-        ItemStack charged;
-        if (this.getChargedItem(itemStack) == this) {
-            charged = new ItemStack(this, 1);
-            ElectricItem.manager.charge(charged, 1.0D / 0.0, 2147483647, true, false);
-            itemList.add(charged);
-        }
+        ItemStack charged = new ItemStack(this, 1);
+        ElectricItem.manager.charge(charged, Double.POSITIVE_INFINITY, Integer.MAX_VALUE, true, false);
+        itemList.add(charged);
 
-        if (this.getEmptyItem(itemStack) == this) {
-            charged = new ItemStack(this, 1);
-            ElectricItem.manager.charge(charged, 0.0D, 2147483647, true, false);
-            itemList.add(charged);
-        }
+        ItemStack itemStack = new ItemStack(this, 1);
+        ElectricItem.manager.charge(charged, 0.0D, Integer.MAX_VALUE, true, false);
+        itemList.add(charged);
     }
 
     /**
      * Determine if the item can be used in a machine or as an armor part to supply energy.
+     *
      * @return Whether the item can supply energy
-     *//*
+     */
     @Override
     public boolean canProvideEnergy(ItemStack itemStack) {
         return false;
     }
 
     /**
-     * Get the item ID to use for a charge energy greater than 0.
-     * @return Item ID to use
-     *//*
-    @Override
-    public Item getChargedItem(ItemStack itemStack) {
-        return this;
-    }
-
-    /**
-     * Get the item ID to use for a charge energy of 0.
-     * @return Item ID to use
-     *//*
-    @Override
-    public Item getEmptyItem(ItemStack itemStack) {
-        return this;
-    }
-
-    /**
      * Get the item's maximum charge energy in EU.
+     *
      * @return Maximum charge energy
-     *//*
+     */
     @Override
     public double getMaxCharge(ItemStack itemStack) {
         return this.maxEnergy;
@@ -91,9 +68,9 @@ public class ItemElectricPL extends ItemPL implements IElectricItem {
     /**
      * Get the item's tier, lower tiers can't send energy to higher ones.
      * Batteries are Tier 1, Energy Crystals are Tier 2, Lapotron Crystals are Tier 3.
-
+     *
      * @return Item's tier
-     *//*
+     */
     @Override
     public int getTier(ItemStack itemStack) {
         return this.tier;
@@ -101,10 +78,11 @@ public class ItemElectricPL extends ItemPL implements IElectricItem {
 
     /**
      * Get the item's transfer limit in EU per transfer operation.
+     *
      * @return Transfer limit
-     *//*
+     */
     @Override
     public double getTransferLimit(ItemStack itemStack) {
         return this.maxPowerTick;
     }
-}*/
+}
--- src/main/java/com/mcgoodtime/productionline/common/items/ItemPLRecord.java
+++ src/main/java/com/mcgoodtime/productionline/common/items/ItemPLRecord.java
@@ -1,82 +1,81 @@
-///*
-// * This file is part of GoodTime-Industrial, licensed under MIT License (MIT).
-// *
-// * Copyright (c) 2015 GoodTime Studio <https://github.com/GoodTimeStudio>
-// * Copyright (c) contributors
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy
-// * of this software and associated documentation files (the "Software"), to deal
-// * in the Software without restriction, including without limitation the rights
-// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// * copies of the Software, and to permit persons to whom the Software is
-// * furnished to do so, subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in
-// * all copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// * THE SOFTWARE.
-// */
-//package com.mcgoodtime.productionline.common.items;
-//
-//import com.mcgoodtime.productionline.common.core.ProductionLine;
-//import net.minecraftforge.fml.common.registry.GameRegistry;
-//import net.minecraftforge.fml.relauncher.Side;
-//import net.minecraftforge.fml.relauncher.SideOnly;
-//import net.minecraft.client.renderer.texture.IIconRegister;
-//import net.minecraft.client.resources.I18n;
-//import net.minecraft.entity.player.EntityPlayer;
-//import net.minecraft.item.ItemRecord;
-//import net.minecraft.item.ItemStack;
-//import net.minecraft.util.ResourceLocation;
-//import net.minecraft.util.StatCollector;
-//
-//import java.util.List;
-//
-//import static com.mcgoodtime.productionline.common.core.ProductionLine.MOD_NAME;
-//import static com.mcgoodtime.productionline.common.core.ProductionLine.RESOURCE_DOMAIN;
-//
-///**
-// * Created by suhao on 2015.6.29.
-// * @author suhao
-// */
-//public class ItemPLRecord extends ItemRecord {
-//
-//    private String name;
-//
-//    public ItemPLRecord(String name) {
-//        super(name);
-//        this.name = name;
-//        this.setCreativeTab(ProductionLine.creativeTabGti);
-//        this.setUnlocalizedName(MOD_NAME + "." + name);
+/*
+ * This file is part of GoodTime-Industrial, licensed under MIT License (MIT).
+ *
+ * Copyright (c) 2015 GoodTime Studio <https://github.com/GoodTimeStudio>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.mcgoodtime.productionline.common.items;
+
+import com.mcgoodtime.productionline.common.core.ProductionLine;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemRecord;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+import java.util.List;
+
+import static com.mcgoodtime.productionline.common.core.ProductionLine.MOD_NAME;
+import static com.mcgoodtime.productionline.common.core.ProductionLine.RESOURCE_DOMAIN;
+
+/**
+ * Created by suhao on 2015.6.29.
+ * @author suhao
+ */
+public class ItemPLRecord extends ItemRecord {
+
+    private String name;
+
+    public ItemPLRecord(String name, SoundEvent soundEvent) {
+        super(name, soundEvent);
+        this.name = name;
+        this.setCreativeTab(ProductionLine.creativeTabGti);
+        this.setUnlocalizedName(MOD_NAME + "." + name);
 //        this.setTextureName(ProductionLine.RESOURCE_DOMAIN + ":" + name);
 //        GameRegistry.registerItem(this, name, MOD_NAME);
-//    }
-//
-//    @Override
-//    public String getRecordNameLocal() {
-//        return StatCollector.translateToLocal(this.getUnlocalizedName() + ".desc");
-//    }
-//
-//    @Override
-//    public ResourceLocation getRecordResource(String name) {
-//        return new ResourceLocation(ProductionLine.RESOURCE_DOMAIN + ":" + this.name);
-//    }
-//
-//    @SideOnly(Side.CLIENT)
-//    @Override
-//    public void addInformation(ItemStack stack, EntityPlayer player, List tooltipList, boolean boo) {
-//        tooltipList.add(I18n.format(this.getUnlocalizedName() + "." + "desc1"));
-//        tooltipList.add(I18n.format(this.getUnlocalizedName() + "." + "desc2"));
-//    }
-//
+    }
+
+    @Override
+    public String getRecordNameLocal() {
+        return net.minecraft.util.text.translation.I18n.translateToLocal(this.getUnlocalizedName() + ".desc");
+    }
+
+    @Override
+    public ResourceLocation getRecordResource(String name) {
+        return new ResourceLocation(ProductionLine.RESOURCE_DOMAIN + ":" + this.name);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List tooltipList, boolean boo) {
+        tooltipList.add(I18n.format(this.getUnlocalizedName() + "." + "desc1"));
+        tooltipList.add(I18n.format(this.getUnlocalizedName() + "." + "desc2"));
+    }
+
 //    @Override
 //    public void registerIcons(IIconRegister iconRegister) {
 //        this.itemIcon = iconRegister.registerIcon(ProductionLine.RESOURCE_DOMAIN + ":" + "record/" + this.name);
 //    }
-//}
+}
